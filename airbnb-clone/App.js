e0caca55d2f41282cc96d3af80e2fe7b4b550042
gon2410x <gonzalo.virtual.2020@gmail.com>
2023-04-10 23:09:36 -0300

    add class 06

--- airbnb-clone/App.js
+++ airbnb-clone/App.js
@@ -4,33 +4,52 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from './src/sreens/home/HomeScreen';
 import { ProfileScreen } from './src/sreens/profile/ProfileScreen';
+import { LocationListScreen } from './src/sreens/location-list/LocationListScreen';
+import { Ionicons } from '@expo/vector-icons';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { COLORS, SPACING } from './src/utils/theme';
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Home: 'home',
+  Profile: 'person',
+  Explorar: 'search'
+}
+
+const screenOptions = ({route}) => {
+  const iconName = TAB_ICON[route.name]  
+  return {
+    tabBarIcon: ({size, color}) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: COLORS.primary,
+    tabBarInactiveTintColor: COLORS.inactive,
+    headerShown: false,
+    tabBarStyle: styles.tabBar
+  }
+}
+
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Funciona mierda</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={screenOptions}>
           <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Explorar" component={LocationListScreen} />
           <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
       </NavigationContainer>
         
       <StatusBar style="auto" />
     </>
-  );
+  )
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  tabBar: {
+    height: SPACING.xxxl,
+    paddingBottom: SPACING.xs,
+    paddingTop: SPACING.xs
+  }
+})
